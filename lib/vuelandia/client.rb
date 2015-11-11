e0790b5221daaaac4c67d499424831e61d90c39b
Mario Muniz <m.muniz@lab.matcom.uh.cu>
2015-11-11 13:26:05 -0500

    Finished perform_availability_request

--- lib/vuelandia/client.rb
+++ lib/vuelandia/client.rb
@@ -12,9 +12,8 @@ module Vuelandia
       freeze
     end
 
-    # each method returns an operation object which contains both the
-    # request and response objects.
-
+    #occupancy must be an array of rooms where each room is a hash with the next structure
+    #{'adult_count'=>x, 'child_count'=>y, 'child_ages'=>[a1,...,an]} , where n = y
     def perform_search_availability(destination:, check_in_date:, check_out_date:, occupancy:, **args)
       builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
         xml.SearchAvailabilityRQ(:version => "2.0", :language => "ENG"){
@@ -38,12 +37,61 @@ module Vuelandia
                 end
               }
             end
+            ######################end of required params#####################
+            xml.HotelList_ args[:hotel_list] if args[:hotel_list]
+            xml.DestinationID_ args[:destination_id] if args[:destination_id]
+            xml.CountryCode_ args[:country_code] if args[:country_code]
+            xml.HotelInformation_ 'Y' if args[:hotel_information]
+            
+            ###################filters description###########################
+            #args[:filters] is a hash with the next structure where each key is optional
+            #{categories:[x1,...,xn], prices:{price_from:x,price_to:y},
+            # room_types:[x1,...,xn], amenities:[x1,...,xn], boards:[x1,...,xn]}
+            filters = args[:filters]
+            if filters
+              xml.Filters{
+                if filters[:categories]
+                  xml.Categories{
+                    filters[:categories].each do |category|
+                      xml.Category_ category
+                    end
+                  }
+                end
+                if filters[:prices]
+                  xml.Prices{
+                    xml.PriceFrom_ filters[:prices][:price_from] if filters[:prices][:price_from] 
+                    xml.PriceTo_ filters[:prices][:price_to] if filters[:prices][:price_to] 
+                  }
+                end
+                if filters[:room_types]
+                  xml.RoomTypes{
+                    filters[:room_types].each do |room_type|
+                      xml.RoomType_ room_type
+                    end
+                  }
+                end
+                if filters[:amenities]
+                  xml.Amenities{
+                    filters[:amenities].each do |amenity|
+                      xml.Amenity_ amenity
+                    end
+                  }
+                end
+                if filters[:boards]
+                  xml.Boards{
+                    filters[:boards].each do |board|
+                      xml.Board_ board
+                    end
+                  }
+                end
+              }
+            end
           }        
         }
     end
     xml = builder.to_xml
     puts xml
-    puts 'requesting'
+    puts 'Requesting'
     connection = Vuelandia::Connection.new(endpoint: configuration.endpoint, 
                                            user: configuration.user, 
                                            password: configuration.password, xmlRQ: xml)  
